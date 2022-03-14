bb65d33d332807c75214208088d38f0649cad3c7
jlese <jackwlesemann@gmail.com>
2022-03-14 19:41:46 -0400

    table creation done

--- renting.sql
+++ renting.sql
@@ -26,8 +26,71 @@ CREATE TABLE IF NOT EXISTS Renter (
     PRIMARY KEY (r_uname)
 );
 
-CREATE TABLE IF NOT EXISTS Admin (
+CREATE TABLE IF NOT EXISTS Administrator (
     a_uname VARCHAR(10) NOT NULL REFERENCES Account(username),
-    region VARCHAR(10) NOT NULL,
+    region VARCHAR(20) NOT NULL,
     PRIMARY KEY (a_uname)
+);
+
+CREATE TABLE IF NOT EXISTS Properties (
+    p_id VARCHAR(10) NOT NULL.
+    addr VARCHAR(10) NOT NULL,
+    num_bathrooms INT NOT NULL,
+    kitchen BIT NOT NULL, -- 0 = no kitchen, 1 = kitchen
+    pool BIT NOT NULL,
+    num_bedrooms INT NOT NULL,
+    rating FLOAT DEFAULT NULL,
+    parking BIT NOT NULL,
+    p_avg_rating FLOAT DEFAULT NULL,
+    PRIMARY KEY (p_id)
+);
+
+CREATE TABLE IF NOT EXISTS AvailableRentals (
+    p_id VARCHAR(10) NOT NULL REFERENCES Properties(p_id),
+    week INT NOT NULL,
+    PRIMARY KEY (p_id)
+
+);
+
+CREATE TABLE IF NOT EXISTS Rents (
+    p_id VARCHAR(10) NOT NULL REFERENCES AvailableRentals(p_id),
+    r_uname VARCHAR(10) NOT NULL REFERENCES Renter(r_uname),
+    contract_id VARCHAR(10) NOT NULL,
+    week INT NOT NULL,
+    PRIMARY KEY (p_id, r_uname, contract_id)
+
+);
+
+CREATE TABLE IF NOT EXISTS Rates (
+    p_id VARCHAR(10) NOT NULL REFERENCES Properties(p_id),
+    h_uname VARCHAR(10) NOT NULL REFERENCES Host(h_uname),
+    r_uname VARCHAR(10) NOT NULL REFERENCES Renter(r_uname),
+    addr VARCHAR(10) NOT NULL REFERENCES Properties(addr),
+    h_rating FLOAT NOT NULL,
+    r_rating FLOAT NOT NULL,
+    p_rating FLOAT NOT NULL,
+    PRIMARY KEY (p_id, h_uname, r_uname)
+    
+);
+
+CREATE TABLE IF NOT EXISTS H_Request (
+    t_id VARCHAR(10) NOT NULL,
+    h_uname VARCHAR(10) NOT NULL REFERENCES Host(h_uname), 
+    a_uname VARCHAR(10) NOT NULL REFERENCES Administrator(a_uname),
+    ymd DATE NOT NULL,
+    region VARCHAR(20) NOT NULL,
+    reason TEXT NOT NULL,
+    PRIMARY KEY (t_id, h_uname, a_uname)
+
+);
+
+CREATE TABLE IF NOT EXISTS R_Request (
+    t_id VARCHAR(10) NOT NULL,
+    r_uname VARCHAR(10) NOT NULL REFERENCES Renter(r_uname), 
+    a_uname VARCHAR(10) NOT NULL REFERENCES Administrator(a_uname),
+    ymd DATE NOT NULL,
+    region VARCHAR(20) NOT NULL,
+    reason TEXT NOT NULL,
+    PRIMARY KEY (t_id, r_uname, a_uname)
+
 );
