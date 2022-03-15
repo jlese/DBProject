f64248e690cf62dd179c329b12b0bda6c4d8b01b
efg2kgw <efg2kgw@virginia.edu>
2022-03-15 16:56:20 -0400

    deletion tiggers and rented trigger
    if host deleted, their properties are deleted, available rentals also deleted
    
    when rental occurs row is removed from available rentals

--- renting.sql
+++ renting.sql
@@ -36,7 +36,8 @@ CREATE TABLE IF NOT EXISTS Administrator (
 );
 
 CREATE TABLE IF NOT EXISTS Properties (
-    p_id VARCHAR(10) NOT NULL.
+    p_id VARCHAR(10) NOT NULL,
+    h_uname VARCHAR(10) NOT NULL REFERENCES Host(h_uname),
     addr VARCHAR(10) NOT NULL,
     price INT NOT NULL CHECK (price > 5),
     num_bathrooms INT NOT NULL,
@@ -56,6 +57,8 @@ CREATE TABLE IF NOT EXISTS AvailableRentals (
     PRIMARY KEY (p_id)
 
 );
+
+
 
 CREATE TABLE IF NOT EXISTS Rents (
     p_id VARCHAR(10) NOT NULL REFERENCES AvailableRentals(p_id),
@@ -100,13 +103,32 @@ CREATE TABLE IF NOT EXISTS R_Request (
 
 );
 
+ALTER TABLE Properties
+ADD CONSTRAINT host_deletion
+FOREIGN KEY (p_id) REFERENCES Properties(p_id)
+ON DELETE CASCADE; 
+
+ALTER TABLE AvailableRentals
+ADD CONSTRAINT property_deletion
+FOREIGN KEY (h_uname) REFERENCES Host(h_uname)
+ON DELETE CASCADE; 
+
+
+
+DELIMITER $$
+CREATE TRIGGER rentedTrigger
+AFTER INSERT ON Rents
+BEGIN
+    DELETE AvailableRentals 
+    WHERE p_id=AvailableRentals.p_id and week=AvailableRentals.week;
+END
+$$
+DELIMITER ;
 
 DELIMITER $$
 CREATE TRIGGER h_rateTrigger
 AFTER INSERT ON Rates
 BEGIN
-    -- SET NOCOUNT ON added to prevent extra result sets from
-    -- interfering with SELECT statements.
     SET NOCOUNT ON;
 
     
@@ -130,6 +152,7 @@ BEGIN
     Update Properties 
         set p_avg_rating = (p_avg_rating*(numRates-1) + Rates.p_rating)/(num_Rates) 
         where Properties.p_id = Rates.p_id;
+    END
 $$
 DELIMITER ;
 
